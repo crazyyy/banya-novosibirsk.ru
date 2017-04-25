a0bc70d09aed9a43f3e2a615b37a55d52c037feb
crazyyy <crazyyy@gmail.com>
2017-04-25 19:44:15 +0300

    cleaned all

--- assets/js/inline_8.js
+++
@@ -1,5 +0,0 @@
- $("#id_rating").rating();
- $(".rating-display-only").rating({
-   displayOnly: true
- });
- $("input[type='tel']").mask("(999)999-99-99");
--- assets/js/inline_9.js
+++
@@ -1,6 +0,0 @@
- $('#read_more').readmore({
-   speed: 75,
-   collapsedHeight: 252,
-   moreLink: '<div class="text-left padding-top-10">\ <a href="#" class="btn btn-read">Узнать больше</a>\ </div>',
-   lessLink: '<div class="text-left padding-top-10">\ <a href="#" class="btn btn-read">Свернуть</a>\ </div>'
- });
